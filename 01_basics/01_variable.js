4050b831008626d3ab591915101f77e419a4b90f
aaryaxz <137858055+aaryaxz@users.noreply.github.com>
2024-05-24 14:29:00 +0000

    Added variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const userName = "Aarya"; 
+let userId = 233;
+var userPassword = "189349"
+userEmail = "Aarya@google.com" // It runs because javascript is very forgiving language.But it is not a good practice
+
+// --------------------------------------------------------------------------------------------------------------------
+
+userName="xyz"
+//constant value which cannnot change.
+userId=444
+userPassword="123"
+userEmail = "xyz@google.com"
+
+/*
+Perfect not use var
+Because of issue in block scope and in functional scope.
+*/
+
+// -------------------------------------------------------------------------------------------------------------------
+
+console.log(userName)
+console.log([userName,userId,userPassword,userEmail])
