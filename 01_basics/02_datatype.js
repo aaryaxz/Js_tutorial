b04170a803e86909eed007c71e069b0f7cc8cdae
aaryaxz <137858055+aaryaxz@users.noreply.github.com>
2024-05-25 10:52:00 +0000

    Datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all code as newer version of js
+
+// alert("Do not open ") // we are using nodejs not browser. We can use it but the syntax is different.
+
+console.log("Hello world")
+
+console.log(
+    3+81
+)  // not a good practice.Code readability shoud be high
+
+
+let name = "Aarya" //String
+let age = 14 // Number
+let isLoggedIn = true // Boolean
+let stage; // undefined
+
+let value = null; // null
+
+// Primitive DataTypes
+
+// number or integers -> 2,32.. max - 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol -> unique
+
+//object
+
+console.log(typeof false)
+console.log(typeof age)
+console.log(typeof stage)
+console.log(typeof value)
