b74e2572d50752e537c378c3182b8a54ed86b86d
Aarya <genzaarya@gmail.com>
2024-06-12 15:53:33 +0530

    array methods

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -14,6 +14,8 @@ console.log(name_list[2])
 const numList = new Array(3,54,90,1.2)
 console.log(numList)
 
+//----------------------------------------------------------------------
+
 // Array Methods
 
 list.push(10,20) // add element at end of list
@@ -38,7 +40,7 @@ console.log("B ",list)
 console.log(list.splice(1,3))
 console.log("C ",list)
 
-// MERGING TWO OR MORE LISTS
+// MERGING TWO OR MORE ARRAY
 
 let marvel_heros = ["Ironman","Thor","Antman"]
 let dc_heros = ["Batman","Flash","Superman"]
@@ -50,7 +52,7 @@ let another_heros= ["Xlr8","AlienX","Greymatter"]
 let heros = marvel_heros.concat(dc_heros,another_heros)
 console.log(heros)
 
-// spread 
+// Spread
 let other_heros = [...dc_heros,...another_heros,...marvel_heros]
 console.log(other_heros)
 
@@ -64,14 +66,21 @@ let name = "Aarya"
 console.log(Array.isArray("Hello World"))
 console.log(Array.from(name))
 console.log(Array(name))
+
 let data = {
-    name : "hitesh",
+    name : "Alok",
     id : 200,
 
 }
-console.log(Array.from({name:"Aarya"}))
+console.log(Object.keys(data)) // converts only keys into array
+console.log(Object.values(data)) // coverts only values into array
+console.log(Object.entries(data).flat(Infinity)) // convert all key values into array
+
 
 let score1 = 100
 let score2= 200
 let score3 = 300
 console.log(Array.of(score1,score2,score3))
+
+console.log(Array(7)) // 7 emplty slots of an array (When we pass single numeric value then it is treated as lenght of the array)
+console.log(Array.of(7)) // 7 as a elem of array [7]
